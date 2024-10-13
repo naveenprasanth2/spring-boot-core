77d04f8227399400946d9f205cf5c6ae94fe7da4
naveen prasanth <naveenprasanth2@gmail.com>
2024-10-13 16:53:44 +0530

    new marker interface with putmapping

--- src/main/java/com/dailycodebuffer/springbootcore/controller/CustomerController.java
+++ src/main/java/com/dailycodebuffer/springbootcore/controller/CustomerController.java
@@ -2,10 +2,12 @@ package com.dailycodebuffer.springbootcore.controller;
 
 import com.dailycodebuffer.springbootcore.dto.CustomerDto;
 import com.dailycodebuffer.springbootcore.marker.PostValidation;
+import com.dailycodebuffer.springbootcore.marker.PutValidation;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,12 @@ public class CustomerController {
 
     @PostMapping("/customer/custom")
     public ResponseEntity<CustomerDto> addCustomer2(@Validated(PostValidation.class) @RequestBody CustomerDto customerDto) {
+        System.out.println(customerDto);
+        return ResponseEntity.ok().body(customerDto);
+    }
+
+    @PutMapping("/customer/custom")
+    public ResponseEntity<CustomerDto> addCustomer3(@Validated(PutValidation.class) @RequestBody CustomerDto customerDto) {
         System.out.println(customerDto);
         return ResponseEntity.ok().body(customerDto);
     }
